9d4e10bdc0ae082aefd2e504cfc706118994dbe4
guoxiaodi <guody001@gmail.com>
2018-11-05 19:11:45 +0800

    调整侧栏菜单

--- src/components/header/asideMenu.vue
+++ src/components/header/asideMenu.vue
@@ -1,57 +1,57 @@
 <template>
-<transition name='fade'>
     <div @click.self="closeMenu" :class="['aside-menu-box']" v-if="showMenu">
-        <div :class="['menu-bar']">
-            <h4>目录</h4>
-            <el-col :span="24">
-                <el-menu
-                default-active="2"
-                class="el-menu-vertical"
-                @open="handleOpen"
-                @close="handleClose"
-                background-color="#45494c"
-                text-color="#fff"
-                active-text-color="#ffd04b">
-                <el-submenu index="1">
-                    <template slot="title">
-                    <span>导航一</span>
-                    </template>
-                    <el-menu-item-group>
-                    <template slot="title">分组一</template>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="1-4">
-                    <template slot="title">选项4</template>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+        <transition name='fade'>
+            <div class="menu-bar" v-if="showMenu">
+                <h4>目录</h4>
+                <el-col :span="24">
+                    <el-menu
+                    default-active="2"
+                    class="el-menu-vertical"
+                    @open="handleOpen"
+                    @close="handleClose"
+                    background-color="#45494c"
+                    text-color="#fff"
+                    active-text-color="#ffd04b">
+                    <el-submenu index="1">
+                        <template slot="title">
+                        <span>导航一</span>
+                        </template>
+                        <el-menu-item-group>
+                        <template slot="title">分组一</template>
+                        <el-menu-item index="1-1">选项1</el-menu-item>
+                        <el-menu-item index="1-2">选项2</el-menu-item>
+                        </el-menu-item-group>
+                        <el-menu-item-group title="分组2">
+                        <el-menu-item index="1-3">选项3</el-menu-item>
+                        </el-menu-item-group>
+                        <el-submenu index="1-4">
+                        <template slot="title">选项4</template>
+                        <el-menu-item index="1-4-1">选项1</el-menu-item>
+                        </el-submenu>
                     </el-submenu>
-                </el-submenu>
-                <el-menu-item index="2">
-                    <span slot="title">导航二</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                    <span slot="title">导航三</span>
-                </el-menu-item>
-                <el-menu-item index="4">
-                    <span slot="title">导航四</span>
-                </el-menu-item>
-                <el-menu-item index="5">
-                    <span slot="title">导航二</span>
-                </el-menu-item>
-                <el-menu-item index="6">
-                    <span slot="title">导航三</span>
-                </el-menu-item>
-                <el-menu-item index="7">
-                    <span slot="title">导航四</span>
-                </el-menu-item>                
-                </el-menu>
-            </el-col>
-        </div>
+                    <el-menu-item index="2">
+                        <span slot="title">导航二</span>
+                    </el-menu-item>
+                    <el-menu-item index="3">
+                        <span slot="title">导航三</span>
+                    </el-menu-item>
+                    <el-menu-item index="4">
+                        <span slot="title">导航四</span>
+                    </el-menu-item>
+                    <el-menu-item index="5">
+                        <span slot="title">导航二</span>
+                    </el-menu-item>
+                    <el-menu-item index="6">
+                        <span slot="title">导航三</span>
+                    </el-menu-item>
+                    <el-menu-item index="7">
+                        <span slot="title">导航四</span>
+                    </el-menu-item>                
+                    </el-menu>
+                </el-col>
+            </div>
+        </transition>
     </div> 
-</transition>
 </template>
 <script>
 export default {
@@ -91,7 +91,7 @@ export default {
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 1000;
+    z-index: 1001;
     touch-action: none;
     width: 100%;
     height: 100%;
@@ -106,9 +106,8 @@ export default {
         width: 270px;
         max-width: 100%;
         height: 100%;
-        background: #25292d;
-        overflow-y: auto;
-        -webkit-overflow-scrolling: touch;
+        background: #45494c;
+
         h4{
             height: 60px;
             width: 270px;
@@ -129,6 +128,9 @@ export default {
 
 }
 .el-col-24{
+    height: 100%;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;    
     box-sizing: border-box;
     padding-top: 60px;
 }
@@ -137,15 +139,13 @@ export default {
 }
 .el-menu{
     border: 0;
+    padding-left:25px;
 }
-.fade-enter-active {
-    transition: opacity 0s
-}
-.fade-leave-active{
-    transition: opacity 2s
+.fade-enter-active, .fade-leave-active{
+    transition: translateX(0) 2s
 }
 .fade-enter, .fade-leave-to{
-    opacity: 0;
+    transition: translateX(-100%) 2s
 }
 </style>
 
