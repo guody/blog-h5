e289ecc31f547c5f29ddd69d7dd7b6ba0f836602
guoxiaodi <guody001@gmail.com>
2018-12-10 19:16:35 +0800

    coding

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store'
-import axios from 'axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/theme-chalk/display.css';
